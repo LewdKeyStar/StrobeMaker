bbcb91a398a3ce45f72d5916603d89e3c75fe0fa
LewdKeyStar <lewdkeystar@gmail.com>
2025-09-20 21:33:28 +0400

    Reworked OrientationPicker to keep dimensions previously entered
    Now, the chosen orientation's dimensions are determined based upon the 
    video's current dimension.
    
    If the two dimensions are equal (after having once chosen the square 
    orientation), the smaller dimension is obtained by reverting to a 16:9 
    aspect ratio.

--- src/ui/atoms/OrientationPicker.py
+++ src/ui/atoms/OrientationPicker.py
@@ -1,7 +1,8 @@
 import flet as ft
 
-WIDTHS = {"landscape": 1920, "portrait": 1080, "square": 1080}
-HEIGHTS = {"landscape": 1080, "portrait": 1920, "square": 1080}
+orientations = ["landscape", "portrait", "square"]
+
+DEFAULT_ASPECT_RATIO_MULT = 0.5625
 
 class OrientationPicker(ft.Row):
     def __init__(
@@ -17,18 +18,33 @@ class OrientationPicker(ft.Row):
 
         self.landscape_button = ft.IconButton(
             icon = ft.icons.CROP_LANDSCAPE,
+            icon_color = ft.colors.GREY_600,
+            selected_icon = ft.icons.CROP_LANDSCAPE,
+            selected_icon_color = ft.colors.BLUE_200,
+
+            selected = True,
 
             on_click = lambda _ : self.set_orientation("landscape")
         )
 
         self.portrait_button = ft.IconButton(
             icon = ft.icons.CROP_PORTRAIT,
+            icon_color = ft.colors.GREY_600,
+            selected_icon = ft.icons.CROP_PORTRAIT,
+            selected_icon_color = ft.colors.BLUE_200,
+
+            selected = False,
 
             on_click = lambda _ : self.set_orientation("portrait")
         )
 
         self.square_button = ft.IconButton(
             icon = ft.icons.CROP_SQUARE,
+            icon_color = ft.colors.GREY_600,
+            selected_icon = ft.icons.CROP_SQUARE,
+            selected_icon_color = ft.colors.BLUE_200,
+
+            selected = False,
 
             on_click = lambda _ : self.set_orientation("square")
         )
@@ -44,6 +60,30 @@ class OrientationPicker(ft.Row):
         )
 
     def set_orientation(self, orientation):
-        self.options.resolution_width = WIDTHS[orientation]
-        self.options.resolution_height = HEIGHTS[orientation]
+
+        getattr(self, f"{orientation}_button").selected = True
+
+        for other_orientation in [ \
+            other_orientation for other_orientation in orientations if other_orientation != orientation \
+        ] :
+            getattr(self, f"{other_orientation}_button").selected = False
+
+        larger_dimension = max(self.options.resolution_width, self.options.resolution_height)
+        smaller_dimension = min(self.options.resolution_width, self.options.resolution_height)
+
+        if smaller_dimension == larger_dimension:
+            smaller_dimension = int(DEFAULT_ASPECT_RATIO_MULT * larger_dimension)
+
+        if orientation == "landscape":
+            self.options.resolution_width = larger_dimension
+            self.options.resolution_height = smaller_dimension
+
+        elif orientation == "portrait":
+            self.options.resolution_width = smaller_dimension
+            self.options.resolution_height = larger_dimension
+
+        else:
+            self.options.resolution_width = larger_dimension
+            self.options.resolution_height = larger_dimension
+
         self.user_on_change(None)
